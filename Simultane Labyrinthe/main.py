d7ce6481efb1c6c7c88d12c2f2d72dd5cf1b15ab
MrQui <timmybrezel@gmail.com>
2025-01-01 15:39:59 +0100

    importing data

---
+++ Simultane Labyrinthe/main.py
@@ -0,0 +1,27 @@
+def read_data_from_file(file_name):
+    with open(file_name, 'r') as file:
+        width, height = map(int, file.readline().split())
+        data = [list(map(int, line.split())) for line in file]
+    return width, height, data
+
+
+def create_matrix(data, height):
+    matrix_horizontal = [data.pop(0) for _ in range(height)]
+    matrix_vertical = [data.pop(0) for _ in range(height - 1)]
+    gruben_anzahl = int(data.pop(0)[0])
+    gruben = [data.pop(0) for _ in range(gruben_anzahl)]
+    return matrix_horizontal, matrix_vertical, gruben
+
+
+
+
+def main():
+    width, height, data = read_data_from_file('labyrinthe1.txt')
+
+    matrix_horizontal_1, matrix_vertical_1, gruben_1 = create_matrix(data, height)
+    matrix_horizontal_2, matrix_vertical_2, gruben_2 = create_matrix(data, height)
+
+
+
+if __name__ == "__main__":
+    main()
