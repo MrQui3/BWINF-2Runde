9e7d231b9591d8cb916448cdfaa3b0537fb25c01
MrQui <timmybrezel@gmail.com>
2025-04-16 17:55:41 +0200

    more plotting data

--- Simultane Labyrinthe/main.py
+++ Simultane Labyrinthe/main.py
@@ -29,7 +29,9 @@ def moving(cost_matrix_1, cost_matrix_2, width, height, horizontal_walls_1, vert
                           horizontal_walls_2, gruben_1, gruben_2, width, height)
 
     moves = [Move([], ((0, 0), (0, 0)), 0, heights_cost=0, weight=0)]
+    n = 0
     while True:
+        n += 1
         move = moves[0]
         moves.pop(0)
 
@@ -40,33 +42,39 @@ def moving(cost_matrix_1, cost_matrix_2, width, height, horizontal_walls_1, vert
         moves = moves[:anzahl]  # Nur die Anzahl besten Züge speichern
 
         if moves[0].cost == 0:
-            return moves[0]
+            return moves[0], n
 
 
 
 def main():
     start_time = time.time()
 
-    width, height, data = read_data_from_file('Simultane Labyrinthe/labyrinthe8.txt')
+    width, height, data = read_data_from_file('labyrinthe8.txt')
 
     horizontal_walls_1, vertical_walls_1, gruben_1 = create_matrix(data, height)
     horizontal_walls_2, vertical_walls_2, gruben_2 = create_matrix(data, height)
 
     cost_matrix_1 = create_matrix_for_laby(horizontal_walls_1, vertical_walls_1, gruben_1, width, height)
     cost_matrix_2 = create_matrix_for_laby(horizontal_walls_2, vertical_walls_2, gruben_2, width, height)
-    print(cost_matrix_1[0][0]+cost_matrix_2[0][0])
 
-    anzahl = 100
-    try:
-        move = moving(cost_matrix_1, cost_matrix_2, width, height, horizontal_walls_1, vertical_walls_1,
-                            horizontal_walls_2, vertical_walls_2, gruben_1, gruben_2, anzahl)
-    except:
-        raise
-        print("No solution found")
-        exit()
+    anzahl = 241
+    anzahl_array = [1, 2, 3, 4, 5, 6, 7, 10, 15, 20, 25,30, 35,40, 45, 50, 55, 65, 70, 75, 80, 85, 90, 95, 100]
+    a = []
+    b = []
+    for i in anzahl_array:
+        try:
+            move, n = moving(cost_matrix_1, cost_matrix_2, width, height, horizontal_walls_1, vertical_walls_1,
+                                horizontal_walls_2, vertical_walls_2, gruben_1, gruben_2, i)
+            a.append(len(move.moves))
+            b.append(n)
+        except:
+            raise
+            print("No solution found")
+            exit()
 
-    print(move.moves)
-    print(len(move.moves))
+        print(i)
+    print(a)
+    print(b)
 
     end_time = time.time()
     print(f"Time: {end_time - start_time}")
--- Simultane Labyrinthe/plotting.py
+++ Simultane Labyrinthe/plotting.py
@@ -2,22 +2,18 @@ import matplotlib
 matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt
 
-anzahl_array = [1, 2, 3, 4, 5, 6, 7, 10, 15, 20, 30, 40, 50, 60, 100]
-moves_array = [19912, 16517, 16290, 15743, 15646, 15716, 15538, 15440, 15359, 15288, 15258, 15104, 15002, 15017, 14872]
-iterations_array = [19912, 23673, 28579, 31526, 34801, 37340, 41943, 52652, 67819, 80367, 108535, 131407, 135790, 137445, 197173]
 
-a = []
-for i in range(len(anzahl_array)):
-    a.append(anzahl_array[i]*(2*10200+10200))
+anzahl_array = [1, 2, 3, 4, 5, 6, 7, 10, 15, 20, 25,30, 35,40, 45, 50, 55, 65, 70, 75, 80, 85, 90, 95, 100]
+moves_array = [5930, 3647, 2914, 2929, 4126, 2738, 2841, 2733, 2566, 2574, 2641, 2146, 2127, 2296, 2271, 2350, 2151, 2125, 2187, 2034, 2204, 2037, 2218, 2153, 2098]
+iterations_array = [5930, 5792, 6423, 9611, 10815, 13065, 15546, 21176, 30359, 39217, 50845, 51589, 57383, 71794, 81137, 95203, 87549, 103736, 122761, 117867, 139066, 126027, 156986, 151542, 155364]
 
 
 plt.plot(anzahl_array, moves_array, marker='', linestyle='-', color='b')
 plt.grid(True)
-plt.xlabel('Begrenzung l')
+plt.xlabel('interationen l')
 plt.ylabel('Anzahl an Schritten')
 plt.show()
 plt.plot(anzahl_array, iterations_array, marker='', linestyle='-', color='b')
-plt.plot(anzahl_array, a, marker='', linestyle='-', color='g')
 
 plt.grid(True)
 plt.xlabel('Begrenzung l')
